4cf8d4408fb4ba56d54cbb0ff284cbd132e7b46b
takahiro <sequence2014@gmail.com>
2017-06-20 13:36:04 +0900

    use slackbot-module written by python

---
+++ chat/slackbot/run.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from slackbot.bot import Bot
+import logging
+
+logging.basicConfig()
+
+def main():
+    bot = Bot()
+    bot.run()
+
+if __name__ == "__main__":
+    print('start slackbot')
+    main()
