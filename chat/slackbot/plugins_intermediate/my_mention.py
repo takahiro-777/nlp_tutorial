b1bdc76783f3c1e1aadc53c50c37217deccf5b34
takahiro <sequence2014@gmail.com>
2017-06-21 19:21:12 +0900

    enable to add_tag to sentence by word2tag_dictionary

--- chat/slackbot/plugins_intermediate/my_mention.py
+++ chat/slackbot/plugins_intermediate/my_mention.py
@@ -2,6 +2,7 @@
 
 import sys
 import MeCab
+import yaml
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
@@ -47,22 +48,28 @@ def listen_func(message):
 
 @default_reply()
 def default_func(message):
+    f = open("word2tag.yml", "r+")
+    word2tag = yaml.load(f)
+
     text = message.body['text']     # メッセージを取り出す
     # 送信メッセージを作る。改行やトリプルバッククォートで囲む表現も可能
     m = MeCab.Tagger ("-Ochasen")
     #m = MeCab.Tagger ("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     m.parse('')
     node = m.parseToNode(text)
-    node = node.next
     #msg = 'あなたの送ったメッセージをmecabで解析します。\n```' + m.parse(text) + '```'
-    if node:
+    tags = []
+    while node:
         word = node.surface
         #品詞を取得
         pos = node.feature.split(",")[1]
+        if word in word2tag:
+            tags.append(word2tag[word])
         #print('{0} , {1}'.format(word, pos))
         #次の単語に進める
-        #node = node.next
-    message.reply("First word is "+word)      # メンション
+        node = node.next
+    message.reply("```Sentence you input is "+text+". Sentence tag is "+','.join(tags)+"```")      # メンション
+    #message.send("Sentence tag is"+','.join(tags)+"```")
 
 #@default_reply()
 #def default_func(message):
