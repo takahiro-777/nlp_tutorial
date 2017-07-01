a8417e104523f437eb0f0e605e5114f0ab9681b6
takahiro <sequence2014@gmail.com>
2017-07-01 09:46:20 +0900

    complete plugin_difficult(janome.Ver)

---
+++ chat/slackbot_janome/README.md
@@ -0,0 +1,25 @@
+# slackbot(pipモジュール)の利用方法
+
+## インストール
+```
+# janomeのインストール
+$ pip install janome
+
+#slackbotのインストール
+$ pip install slackbot
+```
+
+## 使用方法
+```
+$ cd chat/slackbot
+$ cp slackbot_settings_sample.py slackbot_setting.py
+-> API_TOKENを別途取得したAPI_TOKENに変更
+-> PLUGINSから難易度を一つ選択
+$ python run.py
+```
+
+# 参考資料
+```
+http://qiita.com/taroc/items/b9afd914432da08dafc8
+
+```
---
+++ chat/slackbot_janome/plugins_difficult/my_mention.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+import sys
+from janome.tokenizer import Tokenizer
+import yaml
+from slackbot.bot import respond_to     # @botname: で反応するデコーダ
+from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
+from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+
+f = open("plugins_difficult/polarity.yml", "r+")
+polarity = yaml.load(f)
+
+
+@respond_to('メンション')
+def mention_func(message):
+    message.reply('私にメンションと言ってどうするのだ') # メンション
+
+@listen_to('リッスン')
+def listen_func(message):
+    message.send('誰かがリッスンと投稿したようだ')      # ただの投稿
+    message.reply('君だね？')
+
+@listen_to('今日の天気は？')
+def listen_func(message):
+    message.send('晴れのち曇りです。')      # ただの投稿
+
+@listen_to('羊たちの')
+def listen_func(message):
+    message.send('沈黙')      # ただの投稿
+
+@listen_to('mecab')
+def listen_func(message):
+    m = MeCab.Tagger ("-Ochasen")
+    message.send(m.parse ("今日もしないとね"))
+
+@default_reply()
+def default_func(message):
+    #f = open("plugins_difficult/polarity.yml", "r+")
+    #polarity = yaml.load(f)
+
+    text = message.body['text']     # メッセージを取り出す
+    # 送信メッセージを作る。改行やトリプルバッククォートで囲む表現も可能
+    t = Tokenizer()
+    #m = MeCab.Tagger ("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    tokens = t.tokenize(text)
+    #msg = 'あなたの送ったメッセージをmecabで解析します。\n```' + m.parse(text) + '```'
+    pol_val = 0
+    for token in tokens:
+        word = token.surface
+        #品詞を取得
+        pos = token.part_of_speech.split(',')[0]
+        if word in polarity:
+            pol_val = pol_val + float(polarity[word])
+        #print('{0} , {1}'.format(word, pos))
+        #次の単語に進める
+    message.reply("```Sentence you input is "+text+". Sentence polarity is "+str(pol_val)+"```")      # メンション
+    #message.send("Sentence tag is"+','.join(tags)+"```")
+    if pol_val > 0.2:
+        message.react('+1')
+        message.reply("それはいいね！！")
+    elif pol_val < -0.2:
+        message.react('cry')
+        message.reply("そうか、どんまい")
+    else:
+        message.reply("なるほど、そうなんですね〜")
